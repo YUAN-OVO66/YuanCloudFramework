95675c02a0a8c6820f40cd751dec2e53c739c63b
YUAN-OVO66 <1440556275@qq.com>
2025-08-18 16:25:20 +0800

    核心模块初始化 v1.0.2

---
+++ yuan-cloud/cloud-core/src/main/java/com/yuan/cloudcore/CoreApplication.java
@@ -0,0 +1,14 @@
+package com.yuan.cloudcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+        System.out.println();
+        System.out.println("----------------------Cloud-Core核心启动成功 ----------------------");
+        System.out.println();
+    }
+}
